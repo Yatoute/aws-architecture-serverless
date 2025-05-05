d379e8fff9d761b6d2e1e67e2ccd7a0587d1033a
Yatoute <mintoamayatoute@gmail.com>
2025-05-05 02:42:19 +0200

    Initialisation des données dans BucketS3  et dynamodb

---
+++ init_data/data.py
@@ -0,0 +1,60 @@
+import uuid
+
+data = [
+    {
+        'id': f'POST#{uuid.uuid4()}',
+        'user': 'USER#Anya',
+        'title': 'La tueuse en mission',
+        'body': 'Personne ne m’échappera ce soir...',
+        'image': 'https://postagram-bucket20250504214331490400000001.s3.us-east-1.amazonaws.com//103dbabe-cdbc-4477-98c6-596cd05329f0yor-forger.jpg',
+        'label': ['spyxfamily', 'assassine', 'yor']
+    },
+    {
+        'id': f'POST#{uuid.uuid4()}',
+        'user': 'USER#Anya',
+        'title': 'Routine de ménage',
+        'body': 'Une maison propre, une double vie bien rangée !',
+        'image': 'https://postagram-bucket20250504214331490400000001.s3.us-east-1.amazonaws.com//ec3cd817-4655-4e30-9f90-860f716bc7b1all+might.jpeg',
+        'label': ['spyxfamily', 'ménage', 'yor']
+    },
+    {
+        'id': f'POST#{uuid.uuid4()}',
+        'user': 'USER#Deku',
+        'title': 'Je suis là ! 💥',
+        'body': 'Même dans l’ombre, je brillerai pour protéger les innocents.',
+        'image': 'https://postagram-bucket20250504214331490400000001.s3.us-east-1.amazonaws.com//990e5717-a1c1-4caa-8beb-5ae28c2ef462yor+asssassin.jpg',
+        'label': ['mha', 'allmight', 'hero']
+    },
+    {
+        'id': f'POST#{uuid.uuid4()}',
+        'user': 'USER#Link',
+        'title': 'Face au Lynel doré',
+        'body': 'Chaque combat est une épreuve de courage.',
+        'image': 'https://postagram-bucket20250504214331490400000001.s3.us-east-1.amazonaws.com//1ce475b3-af6f-4c3b-b31c-d3acc2379ebcmaster+sword.jpeg',
+        'label': ['zelda', 'bossfight', 'lynel']
+    },
+    {
+        'id': f'POST#{uuid.uuid4()}',
+        'user': 'USER#Link',
+        'title': 'Épée légendaire',
+        'body': 'La lame du héros est à nouveau entre mes mains.',
+        'image': 'https://postagram-bucket20250504214331490400000001.s3.us-east-1.amazonaws.com//f9547e91-431e-425a-894f-e55224c7ef92gundam+quanT.jpg',
+        'label': ['zelda', 'master_sword', 'hero']
+    },
+    {
+        'id': f'POST#{uuid.uuid4()}',
+        'user': 'USER#Setsuna',
+        'title': 'GNT-0000 prêt au combat',
+        'body': 'L’univers est vaste, mais mon devoir est clair.',
+        'image': 'https://postagram-bucket20250504214331490400000001.s3.us-east-1.amazonaws.com//fb85dfdb-c0e9-4fd5-b2da-832610e00303lynel.jpg',
+        'label': ['gundam', 'mecha', 'spatial']
+    },
+    {
+        'id': f'POST#{uuid.uuid4()}',
+        'user': 'USER#Setsuna',
+        'title': 'Fusion énergétique activée',
+        'body': 'Le mode final est enclenché, la bataille commence.',
+        'image': 'https://postagram-bucket20250504214331490400000001.s3.us-east-1.amazonaws.com//a3df4756-399f-4517-bc1e-e387c1352df6trans+AM+00.jpg',
+        'label': ['gundam', 'mecha', 'ultimate_form']
+    }
+]
