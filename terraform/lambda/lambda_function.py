32f037b895da7b553a2a70ba6995be40ae9f472c
Yatoute <mintoamayatoute@gmail.com>
2025-05-07 20:56:18 +0200

    Mise à jour du code source de la lambda

--- terraform/lambda/lambda_function.py
+++ terraform/lambda/lambda_function.py
@@ -1,37 +1,42 @@
 import json
-from urllib.parse import unquote_plus
+# from urllib.parse import unquote_plus
 import boto3
 import os
 import logging
 print('Loading function')
 logger = logging.getLogger()
 logger.setLevel("INFO")
-s3 = boto3.client('s3')
-dynamodb = boto3.resource('dynamodb')
-reckognition = boto3.client('rekognition')
+# s3 = boto3.client('s3')
+# dynamodb = boto3.resource('dynamodb')
+# reckognition = boto3.client('rekognition')
 
-table = dynamodb.Table(os.getenv("table"))
+# table = dynamodb.Table(os.getenv("DYNAMO_TABLE"))
+# bucket = os.getenv("BUCKET")
 
 def lambda_handler(event, context):
-    logger.info(json.dumps(event, indent=2))
-    bucket = ""
-    key = unquote_plus(event) # <- A modifier !!!!
+    # logger.info(json.dumps(event, indent=2))
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps("Tous les messages sont traités")
+    }
+    # key = unquote_plus(event) # <- A modifier !!!!
 
-    # Récupération de l'utilisateur et de l'UUID de la tâche
+    # # Récupération de l'utilisateur et de l'UUID de la tâche
     
 
-    # Ajout des tags user et task_uuid
+    # # Ajout des tags user et task_uuid
 
-    # Appel à reckognition
-    label_data = reckognition.detect_labels(
+    # # Appel à reckognition
+    # label_data = reckognition.detect_labels(
 
-    )
-    logger.info(f"Labels data : {label_data}")
+    # )
+    # logger.info(f"Labels data : {label_data}")
 
-    # Récupération des résultats des labels
+    # # Récupération des résultats des labels
 
 
-    # Mise à jour de la table dynamodb
-    table.update_item(
+    # # Mise à jour de la table dynamodb
+    # table.update_item(
 
-    )
+    # )
