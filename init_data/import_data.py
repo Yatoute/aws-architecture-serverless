9011ddd5d4a30144716259ca1c98df34cfa6da9c
Yatoute <mintoamayatoute@gmail.com>
2025-05-09 20:38:19 +0200

    Mise à jour des données d'initialisation

--- init_data/import_data.py
+++ init_data/import_data.py
@@ -7,29 +7,25 @@ from data import data
 
 bucket = os.getenv("BUCKET")
 table_name = os.getenv("DYNAMO_TABLE")
-
-# Create an S3 resource
 s3 = boto3.resource('s3')
-
-for (dirpath, dirnames, filenames) in os.walk("s3"):
-    if filenames:
-        for filename in filenames:
-            with open(f"{dirpath}/{filename}", 'rb') as file:
-                s3.Object(bucket, f"{'/'.join(dirpath.split(os.path.sep)[1:])}/{filename}").put(Body=file)
-          
-
-# Batch upload
-# Get the service resource.
-# Get the service resource.
 my_config = Config(
     region_name='us-east-1',
     signature_version='v4',
 )
-
 dynamodb = boto3.resource('dynamodb', config=my_config)
+
+# Les posts
 # Get the table.
 table = dynamodb.Table(table_name)
 # Read file
 with table.batch_writer() as batch:
     for row in data:
         batch.put_item(Item=row)
+
+# Uploader les images jointes au posts
+for (dirpath, dirnames, filenames) in os.walk("s3"):
+    if filenames:
+        for filename in filenames:
+            with open(f"{dirpath}/{filename}", 'rb') as file:
+                s3.Object(bucket, f"{'/'.join(dirpath.split(os.path.sep)[1:])}/{filename}").put(Body=file)
+          
